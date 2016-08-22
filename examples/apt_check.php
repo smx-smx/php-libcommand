3f695a2c892b16080beed305d9b3cbb093873e6c
smx-smx <smxdev4@gmail.com>
2016-08-22 18:22:31 +0200

    Added debian update checker/notifier example

---
+++ examples/apt_check.php
@@ -0,0 +1,68 @@
+#!/usr/bin/env php
+<?php
+require_once "log.php";
+require_once "scommand.php";
+require_once "mail.php";
+
+$l = new LogFile(array(
+	Log::LOG_BASENAME => "apt_mon",
+	Log::LOG_DIR => "/var/log/smx/",
+	Log::LOG_EXT => "log",
+	Log::LOG_QUIET => false //print log on stdout
+));
+
+if($l->initLog() === FALSE){
+	fprintf(STDERR, "ERROR: Cannot create log file!\n");
+	return 1;
+}
+
+$c = new SCommand($l);
+$c->setDeps(array("aptitude"));
+
+$h = $c->executeCmd("aptitude update");
+while(!feof($h)){
+	$line = fgets($h);
+	if($line !== FALSE){
+		$l->add($line);
+	}
+}
+$code = TermCommand::destroy($h);
+if($code != 0){
+	$l->add("[-] aptitude update failed!\n");
+	goto sendLog;
+}
+
+$h = $c->executeCmd('aptitude -F"%p|-|%v|-|%V" --disable-columns search ~U');
+$l->add("[+] Available updates:\n\n");
+while(!feof($h)){
+	$line = fgets($h);
+	if($line === FALSE)
+		continue;
+
+	$fields = explode("|-|", trim($line));
+	if(count($fields) !== 3)
+		continue;
+	if($fields[1] == $fields[2])
+		continue;
+	$l->add("%-35s %-30s %-30s\n", $fields[0], $fields[1], $fields[2]);
+}
+
+sendLog:
+$m = new Mail();
+$m->setRecipients(array(
+	"user@example.com"
+));
+
+$m->addAttachment($l->getLogFinalName());
+$m->setSubject("Software Updates Available!");
+
+$body="
+<h2>Hello server administrator</h2>
+<p>New software updates are available</p>
+<p>For details, look at the attached log file</p>
+";
+
+$m->setBody($body);
+$m->send();
+return 0;
+?>
